60adb6ea31cf886cc8846e556c7907b3704deb5a
AzizAlbaqshi <eng.albaqshi@gmail.com>
2021-06-29 18:54:32 +0300

    Delete Poster (back-end ) , Done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const posters = require("./posters");
+let posters = require("./posters");
 const cors = require("cors");
+const { json } = require("express");
 
 const app = express();
 
@@ -11,6 +12,18 @@ app.use(cors());
 app.get("/posters", (req, res) => {
   //JSON
   res.json(posters);
+});
+
+//Delete Route
+app.delete("/posters/:posterId", (req, res) => {
+  const { posterId } = req.params;
+  const foundPoster = posters.find((poster) => poster.id === +posterId);
+  if (foundPoster) {
+    posters = posters.filter((poster) => poster.id !== +posterId);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Poster not Found." });
+  }
 });
 
 app.listen(8000, () => {
