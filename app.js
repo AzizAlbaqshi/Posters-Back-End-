8eb92336c8125a4ad222ff0c15cad6f0c028762a
AzizAlbaqshi <eng.albaqshi@gmail.com>
2021-06-30 17:03:51 +0300

    Create poster , Done

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 let posters = require("./posters");
 const cors = require("cors");
 const { json } = require("express");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 const app = express();
 
 //Middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 //Routes
 app.get("/posters", (req, res) => {
@@ -24,6 +27,19 @@ app.delete("/posters/:posterId", (req, res) => {
   } else {
     res.status(404).json({ message: "Poster not Found." });
   }
+});
+
+//Create Route
+app.post("/posters", (req, res) => {
+  const posterId = posters.length + 1;
+  const posterSlug = slugify(req.body.name, { lower: true });
+  const newPoster = {
+    id: posterId,
+    slug: posterSlug,
+    ...req.body,
+  };
+  posters.push(newPoster);
+  res.status(201).json(newPoster);
 });
 
 app.listen(8000, () => {
